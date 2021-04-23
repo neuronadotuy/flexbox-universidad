9a4c87d0ae19f926a40112ece4e7175e19c4a3b6
Nicolas <nico@neurona.uy>
2021-04-23 16:43:23 -0300

    UDEMY clone, flexbox, sass and gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+/** @format */
+
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+
+function css() {
+  return gulp
+    .src("scss/app.scss")
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 2 versions"],
+        cascade: false,
+      })
+    )
+    .pipe(
+      sass({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(gulp.dest("css"));
+}
+
+function watchFiles() {
+  gulp.watch("scss/*.scss", css);
+}
+
+// Registrar funciones como tareas
+gulp.task("css", css);
+gulp.task("watch", watchFiles);
